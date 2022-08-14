38df64dc8f8856a8d3be0434a8ce003acc914db6
Tianjinxxx <2160543832@qq.com>
2022-08-14 18:10:09 +0800

    Initial commit

---
+++ ssm/src/main/java/com/atguigu/ssm/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.atguigu.ssm.service;
+
+import com.atguigu.ssm.pojo.Employee;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+public interface EmployeeService {
+    /**
+     * 查询所有的员工信息
+     * @return
+     */
+    List<Employee> getAllEmployee();
+
+    PageInfo<Employee> getEmployeePage(Integer pageNum);
+}
